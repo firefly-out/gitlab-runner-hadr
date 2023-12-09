9913c6a48ff427d12878e2371aacf861c8ba9652
Zigelboim Misha <31862542+zigelboim-misha@users.noreply.github.com>
2023-12-09 15:14:43 +0200

    Sidecar command to run indefinitely(#12)
    * Dockerfile to build the CLI
    
    * Sidecar command to run forever
    
    * Dockerfile to allow running the Sidecar command
    
    ---------
    
    Co-authored-by: zigelboim-misha <zigelboim.misha@gmail.com>

--- pkg/requests.go
+++ pkg/requests.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // GetAllRunnerStatutes tries to fetch details about all available runners from the given API url.
@@ -26,4 +27,24 @@ func GetAllRunnerStatutes(url string) (runners []RunnerStatus, err error) {
 	}
 
 	return runners, nil
+}
+
+// SidecarExecutor runs the sidecar for achieving the following goals:
+//   - Getting the status of the pods Runner
+//   - Exporting status using prometheus metrics
+func SidecarExecutor(gitlabBaseUrl, gitlabGroupId string, statusInterval int) {
+	var fullApiUrl = fmt.Sprintf("%s/groups/%s/runners", gitlabBaseUrl, gitlabGroupId)
+	fmt.Printf("Starting the GitLab Runner HADR Sidecar to check for Runner: %s\n"+
+		"Using \"%s\" to check for the runners status\n", "test", fullApiUrl)
+
+	for {
+		body, err := GetAllRunnerStatutes(fullApiUrl)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(body)
+
+		duration := time.Duration(statusInterval) * time.Second // Calculate duration using the VARIABLE value
+		time.Sleep(duration)
+	}
 }
