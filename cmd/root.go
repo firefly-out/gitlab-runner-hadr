c9bcdf683a11486b92ea2a1a3545e6ef297dbae2
Misha <zigelboim.misha@gmail.com>
2023-12-10 15:44:09 +0200

    Adding 2 new flags

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,17 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
 	"runner-hadr/pkg"
+
+	"github.com/spf13/cobra"
 )
 
 var (
 	GitLabUrl           string
 	GitLabGroupId       string
+	GitLabToken         string
+	RunnersPerPage      string
 	StatusCheckInterval int
 
 	rootCmd = &cobra.Command{
@@ -30,7 +33,7 @@ var (
 	Increasing the status check interval to 10 seconds:
 	cli sidecar -i 33 -u http://localhost:8080 -s 10`,
 		Run: func(cmd *cobra.Command, args []string) {
-			pkg.SidecarExecutor(GitLabUrl, GitLabGroupId, StatusCheckInterval)
+			pkg.SidecarExecutor(GitLabUrl, GitLabGroupId, GitLabToken, RunnersPerPage, StatusCheckInterval)
 		},
 	}
 )
@@ -38,12 +41,15 @@ var (
 func Execute() {
 	sidecarCmd.Flags().StringVarP(&GitLabUrl, "gitlab-url", "u", "", "The GitLabs url to check if the runner is connected to it")
 	sidecarCmd.Flags().StringVarP(&GitLabGroupId, "gitlab-group-id", "i", "", "The group ID the runner is installed on")
+	sidecarCmd.Flags().StringVarP(&GitLabToken, "gitlab-token", "t", "", "Access token to read the API")
+	sidecarCmd.Flags().StringVarP(&RunnersPerPage, "runners-per-list", "r", "1000", "How many runners to request from the API")
 	sidecarCmd.Flags().IntVarP(&StatusCheckInterval, "status-check-interval", "s", 5, "Interval for checking the status of the runner in seconds")
 
 	// Add sidecar and decider commands to the root command
 	rootCmd.AddCommand(sidecarCmd)
-	sidecarCmd.MarkFlagRequired("gitlab-url")      // Mark the "gitlab-url" flag as required
-	sidecarCmd.MarkFlagRequired("gitlab-group-id") // Mark the "gitlab-group-id" flag as required
+	sidecarCmd.MarkFlagRequired("gitlab-url")
+	sidecarCmd.MarkFlagRequired("gitlab-group-id")
+	sidecarCmd.MarkFlagRequired("gitlab-token")
 
 	// Start serving the prometheus metrics
 	go pkg.RunUptimeMetrics()
